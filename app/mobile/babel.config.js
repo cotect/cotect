5b3537c446f65172ddb315e8a039cf0259530ddf
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2020-03-23 23:45:10 +0100

    Update case report submission. Use .env file to configure endpoint

--- app/mobile/babel.config.js
+++ app/mobile/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:metro-react-native-babel-preset',
+    'module:react-native-dotenv']
   };
 };
