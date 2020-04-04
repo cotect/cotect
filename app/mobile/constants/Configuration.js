b444f0cd1af981f894f6ec6410ee751ff28ab9b8
Lukas Masuch <lukas.masuch@gmail.com>
2020-04-04 22:23:40 +0200

    Update auto next timeout

--- app/mobile/constants/Configuration.js
+++ app/mobile/constants/Configuration.js
@@ -1,5 +1,5 @@
 export const DEFAULT_COUNTRY_CODE = 'DE';
 export const AUTO_NEXT_ENABLED = true;
-export const AUTO_NEXT_TIMEOUT = 200;
+export const AUTO_NEXT_TIMEOUT = 350;
 export const APP_NAME = "cotect"
 export const OFFLINE_MODE_ENABLED = true;
